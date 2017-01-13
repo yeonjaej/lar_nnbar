b45de22e019ff4fd25307abb9423cdfbd59a5fba
Jeremy Hewes <jeremy.hewes@manchester.ac.uk>
2017-01-13 16:46:17 +0000

    fixed mistake in syntax

--- nnbar/LArCVMaker/LArCVMaker_module.cc
+++ nnbar/LArCVMaker/LArCVMaker_module.cc
@@ -3,7 +3,7 @@
 #include "art/Framework/Core/EDAnalyzer.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
-#include "art/Framework/Services/"
+#include "art/Framework/Services/Optional/TFileService.h"
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 
