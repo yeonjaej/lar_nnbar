7d475c6671491f77f620d67414982eb94938af76
Jeremy Hewes <jeremy.hewes@manchester.ac.uk>
2017-01-25 14:58:01 +0000

    added in a few more includes

--- nnbar/LArCVMaker/LArCVMaker_module.cc
+++ nnbar/LArCVMaker/LArCVMaker_module.cc
@@ -1,8 +1,8 @@
 // framework includes
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Core/EDAnalyzer.h"
-#include "art/Framework/Principal/Event.h"
-#include "art/Framework/Principal/Handle.h"
+//#include "art/Framework/Principal/Event.h"
+//#include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Services/Optional/TFileService.h"
 //#include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
@@ -15,8 +15,12 @@
 #include "TTree.h"
 
 // c++ includes
+#include <vector>
+#include <iterator>
+#include <typeinfo>
 #include <memory>
 #include <string>
+#include <algorithm>
 
 namespace nnbar {
 
