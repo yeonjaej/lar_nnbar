5ac8f6e6b5d141155b995571ae5a57b079fdb3b1
Jeremy Hewes <jeremy.hewes@manchester.ac.uk>
2017-02-13 11:47:39 -0600

    first pass at conversion using larcv iomanager 🐍

--- nnbar/LArCVMaker/LArCVMaker_module.cc
+++ nnbar/LArCVMaker/LArCVMaker_module.cc
@@ -22,8 +22,9 @@
 #include <cmath>
 #include <map>
 
-// external includes
-#include "pngwriter.h"
+// local includes
+#include "Image2D.h"
+#include "IOManager.h"
 
 namespace nnbar {
 
@@ -33,16 +34,18 @@ public:
 
   explicit LArCVMaker(fhicl::ParameterSet const & pset);
   void analyze(art::Event const & evt);
+  void beginJob();
+  void endJob();
 
 private:
 
   void ClearData();
-  void Reset();
-  void FindROI(int apa);
   int FindBestAPA(std::vector<int> apas);
-  void GenerateImage();
+
+  larcv::IOManager fMgr;
 
   std::string fWireModuleLabel;
+  int fMaxTick;
   int fADCCut;
 
   int fFirstWire;
@@ -50,69 +53,41 @@ private:
   int fFirstTick;
   int fLastTick;
 
+  const int fNumberChannels[3] = ( 800, 800, 960 };
+  const int fFirstChannel[3] = { 0, 800, 1600 };
+  const int fLastChannel[3] = { 800, 1600, 2560 };
+
   int fEvent;
   int fAPA;
   int fNumberWires;
   int fNumberTicks;
 
   std::map<int,std::vector<float>> fWireMap;
+
 }; // class LArCVMaker
 
 LArCVMaker::LArCVMaker(fhicl::ParameterSet const & pset) :
     EDAnalyzer(pset),
+    fMgr(pset),
     fWireModuleLabel(pset.get<std::string>("WireModuleLabel")),
+    fMaxTick(pset.get<int>("MaxTick")),
     fADCCut(pset.get<int>("ADCCut"))
 {} // function LArCVMaker::LArCVMaker
+
+void LArCVMaker::beginJob() {
+
+  fMgr.initialize();
+} // function LArCVMaker::beginJob
+
+void LArCVMaker::endJob() {
+
+  fMgr.finalize();
+} // function LArCVMaker::endJob
 
 void LArCVMaker::ClearData() {
 
   fWireMap.clear();
 } // function LArCVMaker::ClearData
-
-void LArCVMaker::Reset() {
-
-  fFirstWire = -1;
-  fLastWire = -1;
-  fFirstTick = -1;
-  fLastTick = -1;
-} // function LArCVMaker::Reset
-
-void LArCVMaker::FindROI(int apa) {
-
-  for (int channel = (2560*apa)+1600; channel < (2560*(apa+1))-1; ++channel) {
-    if (fWireMap.find(channel) != fWireMap.end()) {
-      for (int tick = 0; tick < (int)fWireMap[channel].size(); ++tick) {
-        if (fWireMap[channel][tick] > fADCCut) {
-          if (fFirstWire == -1 || fFirstWire > channel) fFirstWire = channel;
-          if (fLastWire == -1 || fLastWire < channel) fLastWire = channel;
-          if (fFirstTick == -1 || fFirstTick > tick) fFirstTick = tick;
-          if (fLastTick == -1 || fLastTick < tick) fLastTick = tick;
-        }
-      }
-    }
-  }
-
-  fNumberWires = fLastWire - fFirstWire + 1;
-  fNumberTicks = fLastTick - fFirstTick + 1;
-
-  int order = 1;
-  while (fNumberWires/order > 600 || fNumberTicks/order > 600)
-    ++order;
-
-  int margin = 10 * order;
-  if (fFirstWire-margin < (fAPA*2560)+1600) fFirstWire = (fAPA*2560)+1600;
-  else fFirstWire -= margin;
-  if (fLastWire+margin > ((fAPA+1)*2560)-1) fLastWire = ((fAPA+1)*2560)-1;
-  else fLastWire += margin;
-  if (fFirstTick-margin < 0) fFirstTick = 0;
-  else fFirstTick -= margin;
-  if (fLastTick+margin > 4492) fLastTick = 4492;
-  else fLastTick += margin;
-
-  fNumberWires = fLastWire - fFirstWire + 1;
-  fNumberTicks = fLastTick - fFirstTick + 1;
-
-} // function LArCVMaker::FindROI
 
 int LArCVMaker::FindBestAPA(std::vector<int> apas) {
 
@@ -125,12 +100,14 @@ int LArCVMaker::FindBestAPA(std::vector<int> apas) {
 
     FindROI(apa);
 
-    for (int it_x = 0; it_x < fNumberWires; ++it_x) {
-      for (int it_y = 0; it_y < fNumberTicks; ++it_y) {
-        int wire_address = fFirstWire + it_x;
-        int tick_address = fFirstTick + it_y;
-        if (fWireMap.find(wire_address) != fWireMap.end() && tick_address < (int)fWireMap[wire_address].size())
-          max_adc += fWireMap[wire_address][tick_address];
+    for (int it_plane = 0; it_plane < 3; ++it_plane) {
+      for (int it_x = fFirstChannel[it_plane]; it_x < fLastChannel[it_plane]; ++it_x) {
+        for (int it_y = 0; it_y < fMaxTick; ++it_y) {
+          int wire_address = (apa * 2560) + it_x;
+          int tick_address = it_y;
+          if (fWireMap.find(wire_address) != fWireMap.end() && tick_address < (int)fWireMap[wire_address].size())
+            max_adc += fWireMap[wire_address][tick_address];
+        }
       }
     }
 
@@ -143,75 +120,6 @@ int LArCVMaker::FindBestAPA(std::vector<int> apas) {
   return best_apa;
 } // function LArCVMaker::FindBestAPA
 
-void LArCVMaker::GenerateImage() {
-
-  FindROI(fAPA);
-
-  if (fNumberWires < 100 || fNumberTicks < 100) return;
-
-  float min = 0;
-  float max = 0;
-
-  for (int it_x = 0; it_x < fNumberWires; ++it_x) {
-    std::vector<float> wire;
-    for (int it_y = 0; it_y < fNumberTicks; ++it_y) {
-      float adc = 0;
-      int wire_address = fFirstWire + it_x;
-      int tick_address = fFirstTick + it_y;
-      if (fWireMap.find(wire_address) != fWireMap.end() && tick_address < (int)fWireMap[wire_address].size())
-        adc = fWireMap[wire_address][tick_address];
-      if (it_x == 0 && it_y == 0) {
-        min = adc;
-        max = adc;
-      }
-      else if (adc < min) min = adc;
-      else if (adc > max) max = adc;
-    }
-  }
-
-  float base_colour;
-  if (min < 0) base_colour = 1 + (min / (max-min));
-  else base_colour = 1;
-
-  std::vector<std::vector<float>> image;
-  for (int it_x = 0; it_x < fNumberWires; ++it_x) {
-    std::vector<float> wire;
-    for (int it_y = 0; it_y < fNumberTicks; ++it_y) {
-      float adc = base_colour;
-      int wire_address = fFirstWire + it_x;
-      int tick_address = fFirstTick + it_y;
-      if (fWireMap.find(wire_address) != fWireMap.end() && tick_address < (int)fWireMap[wire_address].size())
-        adc = 1 - ((fWireMap[wire_address][tick_address]-min)/(max-min));
-      wire.push_back(adc);
-    }
-    image.push_back(wire);
-  }
-/*
-  std::string filename;
-  char const * outdir = std::getenv("CONDOR_DIR_IMAGE");
-  if (outdir == NULL) {
-    std::cout << "Error! CONDOR_DIR_IMAGE is not set! Exiting..." << std::endl;
-    exit(1);
-  }
-  else filename = std::string(outdir)+"/ImageZ_"+std::to_string(fEvent)+"_"+std::to_string(fAPA)+".png";
-*/
-  std::string filename = "./ImageZ_"+std::to_string(fEvent)+"_"+std::to_string(fAPA)+".png";
-  pngwriter image_maker(fNumberWires, fNumberTicks, base_colour, filename.c_str());
-
-  for (int it_x = 0; it_x < fNumberWires; ++it_x)
-    for (int it_y = 0; it_y < fNumberTicks; ++it_y)
-      image_maker.plotHSV(it_x+1, it_y+1, 0., 0., image[it_x][it_y]);
-
-  image_maker.scale_kxky(4,1);
-  double image_size;
-  if (fNumberWires > fNumberTicks) image_size = fNumberWires;
-  else image_size = fNumberTicks;
-  double scale_factor = 600. / image_size;
-  image_maker.scale_k(scale_factor);
-
-  image_maker.write_png();
-} // function LArCVMaker::GenerateImage
-
 void LArCVMaker::analyze(art::Event const & evt) {
 
   // get event number
@@ -222,26 +130,37 @@ void LArCVMaker::analyze(art::Event const & evt) {
   evt.getByLabel(fWireModuleLabel,wireh);
 
   // initialize ROI finding variables
-  Reset();
   std::vector<int> apas;
 
   // fill wire map
   for (std::vector<recob::Wire>::const_iterator it = wireh->begin();
       it != wireh->end(); ++it) {
     const recob::Wire & wire = *it;
-    if (wire.View() != 2) continue;
     fWireMap.insert(std::pair<int,std::vector<float>>(wire.Channel(),std::vector<float>(wire.Signal())));
     int apa = std::floor(wire.Channel()/2560);
     if (std::find(apas.begin(),apas.end(),apa) == apas.end())
       apas.push_back(apa);
   }
 
-  // find best APA and generate image
+  // find best APA
   int best_apa = FindBestAPA(apas);
   if (best_apa != -1) fAPA = best_apa;
-  GenerateImage();
-  ClearData();
 
+  // produce image
+  auto images = (larcv::EventImage2D*)(fMgr.get_data(::larcv::kProductImage2D, "tpc"))
+  for (int it_plane = 0; it_plane < 3; ++it_plane) {
+    larcv::Image2D image(fNumberChannels,fMaxTick);
+    for (int it_channel = fFirstChannel[it_plane]; it_channel < fLastChannel[it_plane]; ++it_channel) {
+      int channel = it_channel + (fAPA * 2560);
+      for (int it_tick = 0; it_tick < fMaxTick; ++it_tick) {
+        if (fWireMap.find(channel) != fWireMap.end()) image.set_pixel(it_channel,it_tick,fWireMap[channel][it_tick]);
+      }
+    }
+    image.compress(600,600);
+    images->Emplace(std::move(image));
+  }
+
+  fMgr->save_entry();
 } // function LArCVMaker::analyze
 
 DEFINE_ART_MODULE(LArCVMaker)
