a18af404992d97195d2ba8c5673d771b0338e3c0
Jeremy Hewes <jeremy.hewes@manchester.ac.uk>
2017-06-27 09:17:53 -0500

    Changes to DL topology and LArCV maker modules

--- nnbar/DLTopology/DLTopology_module.cc
+++ nnbar/DLTopology/DLTopology_module.cc
@@ -48,6 +48,9 @@ private:
   // Config parameters
   bool fIsSignal;
 
+  // Wire map
+  std::map<int,std::vector<float> > fWireMap;
+
   // Event ID
   int fRun;
   int fSubrun;
@@ -59,7 +62,11 @@ private:
   double fVertexZ;
 
   // Containment
-  bool fContained;
+  int fVertexAPA;          // APA which contains event vertex
+  int fImageAPA;           // APA which is used to create network image
+  bool fChargeContained;   // whether all charge deposition occurs in single APA
+  bool fTracksContained;   // whether all tracks are contained in single APA
+  bool fShowersContained;  // whether all showers are contained in single APA
 
   // Kinematic variables
   int fPionMultiplicity;
@@ -105,7 +112,11 @@ void DLTopology::InitializeBranches() {
   fTree->Branch("VertexY",&fVertexY,"VertexY/D");
   fTree->Branch("VertexZ",&fVertexZ,"VertexZ/D");
 
-  fTree->Branch("Contained",&fContained,"Contained/B");
+  fTree->Branch("VertexAPA",&fVertexAPA,"VertexAPA/I");
+  fTree->Branch("ImageAPA",&fImageAPA,"ImageAPA/I");
+  fTree->Branch("ChargeContained",&fChargeContained,"ChargeContained/B");
+  fTree->Branch("TracksContained",&fTracksContained,"TracksContained/B");
+  fTree->Branch("ShowersContained",&fShowersContained,"ShowersContained/B");
 
   fTree->Branch("PionMultiplicity",&fPionMultiplicity,"PionMultiplicity/I");
   fTree->Branch("LeptonMultiplicity",&fLeptonMultiplicity,"LeptonMultiplicity/I");
@@ -129,6 +140,9 @@ void DLTopology::InitializeBranches() {
 
 void DLTopology::Clear() {
 
+  // Clear wire map
+  fWireMap.clear();
+
   // Clear tree variables at start of event
   fRun = 0;
   fSubrun = 0;
@@ -138,7 +152,11 @@ void DLTopology::Clear() {
   fVertexY = 0;
   fVertexZ = 0;
 
-  fContained = false;
+  fVertexAPA = -1;
+  fImageAPA = -1;
+  fChargeContained = false;
+  fTracksContained = false;
+  fShowersContained = false;
 
   fPionMultiplicity = 0;
   fLeptonMultiplicity = 0;
@@ -194,6 +212,8 @@ void DLTopology::analyze(art::Event const& evt) {
   art::Handle<std::vector<recob::Wire>> WireHandle;
   evt.getByLabel("caldata",WireHandle);
 
+  TVector3 vertex_position;
+
   // Signal topology
   if (fIsSignal) {
     double px = 0;
@@ -204,6 +224,7 @@ void DLTopology::analyze(art::Event const& evt) {
       simb::MCParticle part = mct->GetParticle(it);
       if (part.StatusCode() == 1 && (abs(part.PdgCode()) == 211 || part.PdgCode() == 111)) {
         if (fVertexX == 0 && fVertexY == 0 && fVertexZ == 0) {
+          vertex_position = part.Position(0).Vect();
           fVertexX = part.Position(0).X();
           fVertexY = part.Position(0).Y();
           fVertexZ = part.Position(0).Z();
@@ -266,6 +287,7 @@ void DLTopology::analyze(art::Event const& evt) {
     fMomentumY = py;
     fMomentumZ = pz;
     fInvariantMass = sqrt(pow(e,2)-pow(fMomentum,2));
+    vertex_position = mct->GetNeutrino().Nu().Position(0).Vect();
     fVertexX = mct->GetNeutrino().Nu().Position(0).X();
     fVertexY = mct->GetNeutrino().Nu().Position(0).Y();
     fVertexZ = mct->GetNeutrino().Nu().Position(0).Z();
@@ -277,17 +299,52 @@ void DLTopology::analyze(art::Event const& evt) {
     fLepEnergy = mct->GetNeutrino().Lepton().E();
   }
 
-  // See if event spans multiple apas
+  // Find APA containing vertex
+  art::ServiceHandle<geo::Geometry> geo;
+  for (size_t it_tpc = 0; it_tpc < geo->NTPC(); ++it_tpc) {
+    const geo::TPCGeo & tpc = geo->TPC(it_tpc);
+    fVertexAPA = -1;
+    if (tpc.ContainsPosition(vertex_position)) {
+      fVertexAPA = std::floor((float)it_tpc/2);
+      break;
+    }
+  }
+
+  // Identify APA containing network image, and establish whether event crosses multiple APAs
   std::vector<int> apas;
   for (std::vector<recob::Wire>::const_iterator it = WireHandle->begin(); it != WireHandle->end(); ++it) {
     const recob::Wire & wire = *it;
     int apa = std::floor((float)wire.Channel()/2560.);
     if (std::find(apas.begin(),apas.end(),apa) == apas.end()) apas.push_back(apa);
+    fWireMap.insert(std::pair<int,std::vector<float> >(wire.Channel(),std::vector<float>(wire.Signal())));
   }
+  float best_apa = -1;
+  float best_adc = 0;
+  int number_channels[3] = { 800, 800, 960 };
+  int first_channel[3] = { 0, 800, 1600 };
+  for (int apa : apas) {
+    float max_adc = 0;
+    for (int it_plane = 0; it_plane < 3; ++it_plane) {
+      for (int it_x = 0; it_x < number_channels[it_plane]; ++it_x) {
+        for (int it_y = 0; it_y < 4492; ++it_y) {
+          int w = (apa * 2560) + first_channel[it_plane] + it_x;
+          int t = it_y;
+          if (fWireMap.find(w) != fWireMap.end() && t < (int)fWireMap[w].size())
+            max_adc += fWireMap[w][t];
+        }
+      }
+    }
+    if (max_adc > best_adc) {
+      best_apa = apa;
+      best_adc = max_adc;
+    }
+  }
+  fImageAPA = best_apa;
+  if (apas.size() > 1) fChargeContained = false;
+  else fChargeContained = true;
 
   // Check all tracks are contained in a single APA
-  bool all_particles_contained = true;
-  art::ServiceHandle<geo::Geometry> geo;
+  fTracksContained = true;
   for (std::vector<sim::MCTrack>::const_iterator it = TrackHandle->begin(); it != TrackHandle->end(); ++it) {
     const sim::MCTrack & track = *it;
     bool particle_contained = false;
@@ -298,12 +355,12 @@ void DLTopology::analyze(art::Event const& evt) {
         break;
       }
     }
-    if (particle_contained == false) all_particles_contained = false;
+    if (particle_contained == false) fTracksContained = false;
   }
 
   // Check all showers are contained in a single APA
+  fShowersContained = true;
   for (std::vector<sim::MCShower>::const_iterator it = ShowerHandle->begin(); it != ShowerHandle->end(); ++it) {
-    if (all_particles_contained == false) break;
     const sim::MCShower & shower = *it;
     bool particle_contained = false;
     for (size_t it_tpc = 0; it_tpc < geo->NTPC(); ++it_tpc) {
@@ -313,12 +370,39 @@ void DLTopology::analyze(art::Event const& evt) {
         break;
       }
     }
-    if (particle_contained == false) all_particles_contained = false;
+    if (particle_contained == false) fShowersContained = false;
   }
 
-  // Put it all together to make a containment check
-  if (apas.size() == 1 && all_particles_contained) fContained = true;
-  else fContained = false;
+/*
+  std::vector<double> minx;
+  std::vector<double> maxx;
+  std::vector<double> miny;
+  std::vector<double> maxy;
+  std::vector<double> minz;
+  std::vector<double> maxz;
+  for (size_t it_tpc = 0; it_tpc < geo->NTPC(); ++it_tpc) {
+    const geo::TPCGeo & tpc = geo->TPC(it_tpc);
+    minx.push_back(tpc.MinX());
+    maxx.push_back(tpc.MaxX());
+    miny.push_back(tpc.MinY());
+    maxy.push_back(tpc.MaxY());
+    minz.push_back(tpc.MinZ());
+    maxz.push_back(tpc.MaxZ());
+  }
+  std::cout << "Min X values: ";
+  for (double temp : minx) std::cout << temp << ", ";
+  std::cout << std::endl << "Max X values: ";
+  for (double temp : maxx) std::cout << temp << ", ";
+  std::cout << std::endl << "Min Y values: ";
+  for (double temp : miny) std::cout << temp << ", ";
+  std::cout << std::endl << "Max Y values: ";
+  for (double temp : maxy) std::cout << temp << ", ";
+  std::cout << std::endl << "Min Z values: ";
+  for (double temp : minz) std::cout << temp << ", ";
+  std::cout << std::endl << "Max Z values: ";
+  for (double temp : maxz) std::cout << temp << ", ";
+  std::cout << std::endl;
+*/
 
   // Fill event tree
   fTree->Fill();
